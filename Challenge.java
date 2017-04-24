28a5456b0392964512725fb9b9a3990841f9e3c3
angelalarcon1 <angel.alarcon@jointheleague.org>
2017-04-24 14:53:07 -0700

    Add files via upload

---
+++ Challenge.java
@@ -0,0 +1,48 @@
+package level3;
+
+import processing.core.PApplet;
+
+public class Challenge extends Head {
+	Roomba roomba;
+
+	public static void main(String[] args) {
+		PApplet.main("level3.Processing");
+	}
+
+	public Challenge(Roomba roomba) {
+		super(roomba);
+	}
+
+	public void initialize() {
+		driveDirect(500, 500);
+		sleep(2000);
+		driveDirect(90, 500);	
+		sleep(600);
+		driveDirect(300, 300);	
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+	public void loop() {
+	
+	}
+}
